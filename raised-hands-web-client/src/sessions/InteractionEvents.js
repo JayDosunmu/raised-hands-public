ea05648ccdbb966532a598afc86258d964cee71e
Faizon Williams <faizon@protonmail.com>
2020-04-21 17:11:00 -0400

    Added trophy image to leader

--- raised-hands-web-client/src/sessions/InteractionEvents.js
+++ raised-hands-web-client/src/sessions/InteractionEvents.js
@@ -11,17 +11,17 @@ render() {
     return (
 
 <div className = "Textbox_at_Bottom">
- <footer className = "fixed-bottom offset-sm-2">
-<div class="col-md-12">
-<div class="input-group mb-3">
-  <div class="input-group-prepend">
-    <button class="btn btn-secondary active" type="button">Submit</button>
-  </div>
-  <input type="text" class="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1">
-      </input>
-</div>
-</div>
-</footer> 
+    <footer className = "fixed-bottom offset-sm-2">
+        <div class="col-md-12">
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <button class="btn btn-secondary active" type="button">Submit</button>
+                        </div>
+                             <input type="text" class="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1">
+                              </input>
+                </div>
+        </div>
+    </footer> 
 </div>  
     );
 }
--- raised-hands-web-client/src/sessions/SessionParticipantCard.js
+++ raised-hands-web-client/src/sessions/SessionParticipantCard.js
@@ -1,27 +1,21 @@
 import React from 'react';
+import Image from './Assets/trophy.svg'
 
 export default class SessionParticipateCard extends React.Component {
     constructor(props) {
         super(props);
         console.log(props);
     }
-
-    render() {
-
-        let name = this.props.participant.account.name;
-        let leaderStatus ;
-        
-        if((this.props.participant.leader)){
-             leaderStatus = "Leader ";
-        }
     
+    render() {
+        let name = this.props.participant.account.name;
+        
         return (
-            
+  
                 <div className="card border-dark mb-3" style={{ maxWidth: '12em', maxHeight: '5em',}}>
                     <div className="card-body">
                         <h5 >{name}</h5>
-                        {this.props.participant.leader && <h5 className="card-title">{leaderStatus} </h5>}
-                        
+                        {this.props.participant.leader && <img src={Image}></img>} 
                     </div>
                 </div>
        
