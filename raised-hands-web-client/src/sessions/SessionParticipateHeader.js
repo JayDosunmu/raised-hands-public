8ca35c37f7ca64ac65fcd6c8d1a11b94b9dc351b
Taslim Dosunmu <tjdosunmu@gmail.com>
2020-04-28 23:33:14 -0400

    update session ui

--- raised-hands-web-client/src/sessions/SessionParticipateHeader.js
+++ raised-hands-web-client/src/sessions/SessionParticipateHeader.js
@@ -3,11 +3,11 @@ import React from 'react';
 import { SessionService, SessionIdDisplay } from '.';
 
 export default ({ session, participant }) => {
-    const { joinId = '...loading' , passcode = '...loading', sessionId = -1, active = true } = session || {};
+    const { joinId = 'loading...' , passcode = 'loading...', sessionId = -1, active = true, name = 'loading...' } = session || {};
     return (
         <div className="col">
             <h4>
-                Session -- Join ID: <SessionIdDisplay joinId={joinId} /> || passcode: {passcode}
+                {name} -- Join ID: <SessionIdDisplay joinId={joinId} /> || passcode: {passcode}
             </h4>
             { participant && participant.leader &&
                 <button onClick={() => SessionService.changeSessionActive(sessionId, !active)} className="btn btn-primary">
